773037faf3e6e1d9a352191432874af7809e0487
Pavel Kopecký <pavel.kopecky@icloud.com>
2023-09-20 10:52:49 +0200

    switch to express.static for static sites

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,16 @@
 import * as express from 'express';
 import {User} from "./User.js";
 const path = require('path');
-const app = express();
 
+const app = express();
 const userList : User[] = [];
 
 app.set('view engine', 'html');
 //grants access to the forms in the html files through the req variables
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res, next) => {
-    res.sendFile(__dirname + '/views/index.html');
-})
-
-app.get('/register', (req, res, next) => {
-    res.sendFile(__dirname + '/views/register.html');
-})
-
-app.get('/login', (req, res, next) => {
-    res.sendFile(__dirname + '/views/login.html');
-})
+app.use(express.static(__dirname + '/../views', {
+    extensions: [ 'html' ]
+}));
 
 app.listen(3000);
