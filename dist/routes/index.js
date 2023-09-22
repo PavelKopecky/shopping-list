940c626ac3a97eafe49fd39147228f8cba537fa2
Pavel Kopecký <pavel.kopecky@icloud.com>
2023-09-22 21:19:56 +0200

    add hbs to all html files and add a separate routing file

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,4 +1,86 @@
 "use strict";
-const express = require('express');
-const router = express.Router();
-module.exports = router;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.setup = void 0;
+const User_1 = require("../User");
+const express = require("express");
+const bcrypt = require('bcrypt');
+let userList = [];
+const setup = (app, router) => {
+    app.use(express.static(__dirname + '/../views', {
+        extensions: ['hbs']
+    }));
+    router.get('/', (req, res, next) => {
+        console.log('/');
+        return req.session.authorized ? res.render('main', { username: req.session.username, title: 'Home page' }) : next();
+    });
+    router.get('/login', (req, res, next) => {
+        console.log('/login');
+        return req.session.authorized ? res.render('main', { username: req.session.username, title: 'Login' }) :
+            res.render('login');
+    });
+    router.get('/register', (req, res, next) => {
+        console.log('/register');
+        return res.render('register', { username: req.session.username, title: 'Register' });
+    });
+    app.post('/login', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+        if (req.session.authorized) {
+            return res.redirect('/');
+        }
+        const user = userList.find((user) => req.body.username === user.name);
+        if (!user) {
+            console.log('user not found');
+            res.redirect('/login');
+        }
+        else {
+            bcrypt.compare(req.body.password, user.hashedPassword, (e, data) => {
+                if (e) {
+                    console.log(e);
+                }
+                if (data) {
+                    console.log('login successful');
+                    req.session.username = user.name;
+                    req.session.authorized = true;
+                    res.redirect('/');
+                    return;
+                }
+                else {
+                    console.log('passwords do not match');
+                    res.redirect('/login');
+                    return;
+                }
+            });
+        }
+    }));
+    app.post('/register', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+        try {
+            const hashedPassword = yield bcrypt.hash(req.body.password, 10);
+            userList.push(new User_1.User(req.body.username, hashedPassword));
+            res.redirect('/login');
+            next();
+        }
+        catch (e) {
+            console.log(e);
+            res.redirect('/register');
+        }
+    }));
+    app.post('/logout', function (req, res, next) {
+        console.log('user logged out');
+        req.session.username = null;
+        req.session.authorized = false;
+        req.session.save(function () {
+            req.session.regenerate(function () {
+                res.redirect('/login');
+            });
+        });
+    });
+};
+exports.setup = setup;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import {User} from "./User.js";
+import {setup} from './routes/index.js';
 
 const bcrypt = require('bcrypt');
 const path = require('path');
 const session = require('express-session');
 const { engine } = require('express-handlebars');
+
 
 const app = express();
 const router = express.Router();
@@ -20,91 +22,12 @@ app.use(session({
 //grants access to the forms in the html files through the req variables
 app.use(express.urlencoded({extended: false}));
 
-app.engine('html', engine({extname: 'html', defaultLayout: 'index', layoutsDir: __dirname + '/../views'}));
+app.engine('hbs', engine({extname: 'hbs', defaultLayout: 'index', layoutsDir: __dirname + '/../views/layouts'}));
 //replaces app.get, loads static pages from the directory automatically
-app.set('view engine', 'html');
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname + '/../views'));
 app.use('/', router);
-
-router.get('/', (req, res, next) => {
-    console.log('reached, ' + req.session!.username);
-    return req.session!.authorized ? res.render('index', {username: req.session!.username}) : next();
-})
-
-app.use('/', (req, res, next) => {
-    if (req.path !== '/') {
-        return req.path === '/login' && req.session!.authorized ?
-            res.redirect('/') : next();
-    } else {
-        if (req.session!.authorized) {
-            return next();
-        } else {
-            console.log('not verified');
-            res.redirect('/register');
-        }
-    }
-});
-
-app.use(express.static(__dirname + '/../views', {
-    extensions: [ 'html' ]
-}));
-
-app.post('/login', async (req, res, next) => {
-
-    if (req.session!.authorized) {
-        return res.redirect('/');
-    }
-
-    const user = userList.find((user) => req.body.username === user.name);
-
-    if (!user) {
-        console.log('user not found');
-        res.redirect('/login');
-    } else {
-        bcrypt.compare(req.body.password, user.hashedPassword, (err, data) => {
-            if (err){
-                console.log('err');
-            }
-            if (data) {
-                console.log('login successful');
-                req.session!.username = user.name;
-                req.session!.authorized = true;
-                res.redirect('/');
-                return;
-            } else {
-                console.log('passwords do not match');
-                res.redirect('/login');
-                return;
-            }
-        });
-    }
-});
-
-app.post('/register', async (req, res, next) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        userList.push(new User(
-            req.body.username,
-            hashedPassword
-        ));
-        res.redirect('/login');
-        next();
-    } catch {
-        console.log('reg err');
-        res.redirect('/register');
-    }
-});
-
-app.post('/logout', function (req, res, next) {
-    console.log('user logged out');
-    req.session!.username = null;
-    req.session!.authorized = false;
-    req.session!.save(function () {
-        req.session!.regenerate(function () {
-            res.redirect('/login')
-        })
-    })
-})
+setup(app, router);
 
 module.exports = router;
 
